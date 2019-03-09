a7626efb5c9b5fb46b841dfb4c04f694165756ae
gness1804 <graham.nessler@gmail.com>
2019-03-09 16:17:08 -0600

    Refactor to simplify

--- src/App.js
+++ src/App.js
@@ -4,56 +4,34 @@ import axios from 'axios';
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [query, setQuery] = useState('');
-  const [finalQuery, setFinalQuery] = useState('');
 
-  const url = `http://hn.algolia.com/api/v1/search?query=${finalQuery}`;
+  const url = `http://hn.algolia.com/api/v1/search?query=${query}`;
 
-  const getArticles = async () => {
-    if (!finalQuery) {
+  const getArticles = async event => {
+    event.preventDefault();
+    if (!query) {
+      alert('You must enter a query.');
       return;
     }
     const response = await axios.get(url);
     setArticles(response.data.hits);
   };
 
-  useEffect(() => {
-    try {
-      getArticles();
-    } catch (err) {
-      /* eslint-disable no-console */
-      console.error(
-        `Error in fetching data: ${err.message || JSON.stringify(err)}`,
-      );
-      /* eslint-enable no-console */
-    }
-  }, [finalQuery]);
-
   const handleUpdateSearchText = event => {
     setQuery(event.target.value);
   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    if (!query) {
-      alert('You must enter a query.');
-      return;
-    }
-    setFinalQuery(query);
-  };
-
   return (
     <>
-      <form>
+      <form onSubmit={getArticles}>
         <input
           type="text"
           placeholder="Query"
           onChange={handleUpdateSearchText}
         />
-        <button type="submit" onClick={handleSubmit}>
-          Search
-        </button>
-        {finalQuery ? (
-          <p>Showing results for: {finalQuery}</p>
+        <button type="submit">Search</button>
+        {articles.length ? (
+          <p>Showing results for: {query}</p>
         ) : (
           <p>No results. Enter a search term now!</p>
         )}
